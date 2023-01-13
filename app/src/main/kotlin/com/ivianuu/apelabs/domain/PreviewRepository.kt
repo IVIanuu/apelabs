fe3675ce2fb56188a6bbcee691c2af70c4c1fdcf
manu <IVIanuu@gmail.com>
2023-01-13 16:49:24 +0100

    Reduced stress when changing previews

--- app/src/main/kotlin/com/ivianuu/apelabs/domain/PreviewRepository.kt
+++ app/src/main/kotlin/com/ivianuu/apelabs/domain/PreviewRepository.kt
@@ -3,21 +3,29 @@ package com.ivianuu.apelabs.domain
 import com.ivianuu.apelabs.data.Program
 import com.ivianuu.essentials.AppScope
 import com.ivianuu.essentials.coroutines.bracket
+import com.ivianuu.essentials.coroutines.share
 import com.ivianuu.essentials.logging.Logger
+import com.ivianuu.essentials.time.milliseconds
+import com.ivianuu.essentials.time.seconds
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.common.Scoped
 import com.ivianuu.injekt.coroutines.NamedCoroutineScope
 import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
@@ -45,7 +53,12 @@ context(Logger, NamedCoroutineScope<AppScope>)
       }
     }
     .distinctUntilChanged()
-    .shareIn(this@NamedCoroutineScope, SharingStarted.WhileSubscribed(), 1)
+    .conflate()
+    .transform {
+      emit(it)
+      delay(200.milliseconds)
+    }
+    .share(SharingStarted.WhileSubscribed(), 1)
 
   suspend fun updatePreviewsEnabled(value: Boolean) {
     _previewsEnabled.value = value
