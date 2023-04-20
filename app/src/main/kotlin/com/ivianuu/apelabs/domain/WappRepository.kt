bbdcdd88805db78eb0f1093bca59cb4adf244387
manu <IVIanuu@gmail.com>
2023-04-20 12:01:19 +0200

    Improved wapp discovery

--- app/src/main/kotlin/com/ivianuu/apelabs/domain/WappRepository.kt
+++ app/src/main/kotlin/com/ivianuu/apelabs/domain/WappRepository.kt
@@ -105,26 +105,20 @@ import kotlinx.coroutines.sync.withLock
           foundWapps += wapp
           if (wapp in wapps)
             return@launch
-        }
-
-        wappRemote.withWapp(wapp.address) {
-          wappsLock.withLock {
-            if (wapp in wapps)
-              return@withWapp
-
+          else {
             logger { "${wapp.debugName()} add wapp" }
 
             wapps += wapp
             trySend(wapps.toList())
           }
+        }
 
-          onCancel(block = { awaitCancellation() }) {
-            if (coroutineContext.isActive) {
-              logger { "${wapp.debugName()} remove wapp" }
-              wappsLock.withLock {
-                wapps.remove(wapp)
-                trySend(wapps.toList())
-              }
+        wappRemote.withWapp<Unit>(wapp.address) {
+          onCancel {
+            logger { "${wapp.debugName()} remove wapp" }
+            wappsLock.withLock {
+              wapps.remove(wapp)
+              trySend(wapps.toList())
             }
           }
         }
