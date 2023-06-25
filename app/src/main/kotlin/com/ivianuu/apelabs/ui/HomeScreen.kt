00681e062046f2d34cdbf48a2b339e9480b45a43
manu <ivianuu@gmail.com>
2023-06-25 15:56:04 +0200

    ...

--- app/src/main/kotlin/com/ivianuu/apelabs/ui/HomeScreen.kt
+++ app/src/main/kotlin/com/ivianuu/apelabs/ui/HomeScreen.kt
@@ -182,9 +182,7 @@ import kotlin.math.roundToInt
       }
     }
   ) {
-    LazyVerticalGrid(
-      columns = GridCells.Fixed(2)
-    ) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
       if (model.selectedGroups.isEmpty()) {
         item(span = { GridItemSpan(maxLineSpan) }) {
           Text(
